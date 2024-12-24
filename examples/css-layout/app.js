abc8668d56a6c970d10170198ce07a24ae9ec50f
wyknmjj <wyknmjj@protonmail.com>
2024-12-24 16:31:09 +0800

    basic fix

--- examples/css-layout/app.js
+++ examples/css-layout/app.js
@@ -1,6 +1,6 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var ReactCanvas = require('react-canvas');
+var React = require("react");
+var ReactDOM = require("react-dom");
+var ReactCanvas = require("react-canvas");
 
 var Surface = ReactCanvas.Surface;
 var Group = ReactCanvas.Group;
@@ -9,24 +9,40 @@ var Text = ReactCanvas.Text;
 var FontFace = ReactCanvas.FontFace;
 
 var App = React.createClass({
-
   componentDidMount: function () {
-    window.addEventListener('resize', this.handleResize, true);
+    window.addEventListener("resize", this.handleResize, true);
   },
 
   render: function () {
     var size = this.getSize();
     return (
-      <Surface top={0} left={0} width={size.width} height={size.height} enableCSSLayout={true}>
+      <Surface
+        top={0}
+        left={0}
+        width={size.width}
+        height={size.height}
+        enableCSSLayout={true}
+      >
         <Group style={this.getPageStyle()}>
-          <Text style={this.getTitleStyle()}>
-            Professor PuddinPop
-          </Text>
+          <Text style={this.getTitleStyle()}>Professor PuddinPop</Text>
           <Group style={this.getImageGroupStyle()}>
-            <Image src='https://placekitten.com/720/840' style={this.getImageStyle()} fadeIn={true} />
+            <Image
+              src="https://picsum.photos/720/840"
+              style={this.getImageStyle()}
+              fadeIn={true}
+            />
           </Group>
           <Text style={this.getExcerptStyle()}>
-            With these words the Witch fell down in a brown, melted, shapeless mass and began to spread over the clean boards of the kitchen floor.  Seeing that she had really melted away to nothing, Dorothy drew another bucket of water and threw it over the mess.  She then swept it all out the door.  After picking out the silver shoe, which was all that was left of the old woman, she cleaned and dried it with a cloth, and put it on her foot again.  Then, being at last free to do as she chose, she ran out to the courtyard to tell the Lion that the Wicked Witch of the West had come to an end, and that they were no longer prisoners in a strange land.
+            With these words the Witch fell down in a brown, melted, shapeless
+            mass and began to spread over the clean boards of the kitchen floor.
+            Seeing that she had really melted away to nothing, Dorothy drew
+            another bucket of water and threw it over the mess. She then swept
+            it all out the door. After picking out the silver shoe, which was
+            all that was left of the old woman, she cleaned and dried it with a
+            cloth, and put it on her foot again. Then, being at last free to do
+            as she chose, she ran out to the courtyard to tell the Lion that the
+            Wicked Witch of the West had come to an end, and that they were no
+            longer prisoners in a strange land.
           </Text>
         </Group>
       </Surface>
@@ -37,59 +53,59 @@ var App = React.createClass({
   // ======
 
   getSize: function () {
-    return document.getElementById('main').getBoundingClientRect();
+    return document.getElementById("main").getBoundingClientRect();
   },
 
   getPageStyle: function () {
     var size = this.getSize();
     return {
-      position: 'relative',
+      position: "relative",
       padding: 14,
       width: size.width,
       height: size.height,
-      backgroundColor: '#f7f7f7',
-      flexDirection: 'column'
+      backgroundColor: "#f7f7f7",
+      flexDirection: "column",
     };
   },
 
   getImageGroupStyle: function () {
     return {
-      position: 'relative',
+      position: "relative",
       flex: 1,
-      backgroundColor: '#eee'
+      backgroundColor: "#eee",
     };
   },
 
   getImageStyle: function () {
     return {
-      position: 'absolute',
+      position: "absolute",
       left: 0,
       top: 0,
       right: 0,
-      bottom: 0
+      bottom: 0,
     };
   },
 
   getTitleStyle: function () {
     return {
-      fontFace: FontFace('Georgia'),
+      fontFace: FontFace("Georgia"),
       fontSize: 22,
       lineHeight: 28,
       height: 28,
       marginBottom: 10,
-      color: '#333',
-      textAlign: 'center'
+      color: "#333",
+      textAlign: "center",
     };
   },
 
   getExcerptStyle: function () {
     return {
-      fontFace: FontFace('Georgia'),
+      fontFace: FontFace("Georgia"),
       fontSize: 17,
       lineHeight: 25,
       marginTop: 15,
       flex: 1,
-      color: '#333'
+      color: "#333",
     };
   },
 
@@ -98,8 +114,7 @@ var App = React.createClass({
 
   handleResize: function () {
     this.forceUpdate();
-  }
-
+  },
 });
 
-ReactDOM.render(<App />, document.getElementById('main'));
+ReactDOM.render(<App />, document.getElementById("main"));
--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,39 @@
-var gulp = require('gulp');
-var del = require('del');
-var connect = require('gulp-connect');
-var webpack = require('webpack-stream');
-var webpackConfig = require('./webpack.config.js');
+var gulp = require("gulp");
+var del = require("del");
+var connect = require("gulp-connect");
+var webpack = require("webpack-stream");
+var webpackConfig = require("./webpack.config.js");
 
 var port = process.env.PORT || 8080;
 var reloadPort = process.env.RELOAD_PORT || 35729;
 
-gulp.task('clean', function () {
-  del(['build']);
+gulp.task("clean", function () {
+  del(["build"]);
 });
 
-gulp.task('build', function () {
-  return gulp.src(webpackConfig.entry.timeline[0])
+gulp.task("build", function () {
+  return gulp
+    .src(webpackConfig.entry.timeline[0])
     .pipe(webpack(webpackConfig))
-    .pipe(gulp.dest('build/'));
+    .pipe(gulp.dest("build/"));
 });
 
-gulp.task('serve', function () {
+gulp.task("serve", function () {
   connect.server({
+    host: "0.0.0.0",
     port: port,
     livereload: {
-      port: reloadPort
-    }
+      port: reloadPort,
+    },
   });
 });
 
-gulp.task('reload-js', function () {
-  return gulp.src('./build/*.js')
-    .pipe(connect.reload());
+gulp.task("reload-js", function () {
+  return gulp.src("./build/*.js").pipe(connect.reload());
 });
 
-gulp.task('watch', function () {
-  gulp.watch(['./build/*.js'], ['reload-js']);
+gulp.task("watch", function () {
+  gulp.watch(["./build/*.js"], ["reload-js"]);
 });
 
-gulp.task('default', ['clean', 'build', 'serve', 'watch']);
+gulp.task("default", ["clean", "build", "serve", "watch"]);
